725b4ee459ac89a3e7fbe364b3af56eb910aec66
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2023-03-19 22:01:47 -0300

    Create module.js

---
+++ src/module/club/module.js
@@ -0,0 +1,3 @@
+const ClubController = require('./controller/clubController');
+const ClubRepository = require('./repository/json/clubRepository');
+const ClubService = require('./service/clubService');
