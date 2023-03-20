fbd1851b7c2acac430e76e59a2e3208c2b9ae682
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2023-03-19 22:01:33 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const configureDependencyInjection = require('./config/di');
+const { init: initClubModule } = require('./module/club/module');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use('/public', express.static('public'));
+
+nunjucks.configure('src/module',{
+    autoescape: true,
+    express: app,   
+});
+
+const container = configureDependencyInjection(app);
+app.use(container.get('Session'));
+
+initClubModule(app, container);
+
+/**
+ * @type {import('./module/club/controller/clubController')} controller;
+ */
+const clubController = container.get('ClubController');
+app.get('/', clubController.index.bind(clubController));
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
